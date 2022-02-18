fc87ade0e8974ba8842d31d0a39ce1666e34f895
lmchun <lmchun@sbcglobal.net>
2022-02-18 16:04:49 -0500

    adding in folders for the upcoming weeks

---
+++ wk4-playground/wk4-inclass-sound-ui/Shared/wk4_inclass_sound_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  wk4_inclass_sound_uiApp.swift
+//  Shared
+//
+//  Created by Chris Chun on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct wk4_inclass_sound_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
